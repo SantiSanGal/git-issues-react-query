7147c6b19035ab106c96fcc5ba6d8883c79f910d
SantiSanGal <santiagon.g.m@gmail.com>
2023-08-19 18:41:38 -0400

    placeholderData e initialData

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -9,6 +9,8 @@ const getLabels = async():Promise<Label[]> => {
     await sleep(2);
 
     const { data } = await gitHubApi.get<Label[]>('/labels');
+    console.log(data);
+    
     return  data;
 }
 
@@ -25,7 +27,43 @@ export const useLabels = () => {
             // stale = estado de información no confiable, que puede ser desactualizada
             staleTime: 1000 * 60 * 60, //cada 1 hora se cargará nueva información o al cargar por primera vez
             
-            placeholderData: []
+            //muestra por defecto
+            initialData: [
+            {
+                id: 725156255,
+                node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+                url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+                name: "good first issue (taken)",
+                color: "b60205",
+                default: false
+            }, 
+            {
+                id: 717031390,
+                node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+                url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+                name: "good first issue",
+                color: "6ce26a",
+                default: true
+            }],
+
+            //en vez de la pantalla de carga, muestra los objetos como predefinidos, hasta que llegue la nueva data
+            placeholderData: [
+            {
+                id: 725156255,
+                node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+                url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+                name: "good first issue (taken)",
+                color: "b60205",
+                default: false
+            }, 
+            {
+                id: 717031390,
+                node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+                url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+                name: "good first issue",
+                color: "6ce26a",
+                default: true
+            }]
         }
     );
 

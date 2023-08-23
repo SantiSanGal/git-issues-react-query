276580afa48fcd739c984c66b52be4209eb25995
SantiSanGal <santiagon.g.m@gmail.com>
2023-08-22 22:45:56 -0400

    issueItem done

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const gitHubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: 'Bearer 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'
     }
 });
